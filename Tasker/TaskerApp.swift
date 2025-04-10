83446c2bf95d92392b34608495b822d58d147ecc
Kodi Maberek <138794715+KodiMaberek@users.noreply.github.com>
2025-04-10 11:22:58 -0700

    Update tasker

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Rodion Akhmedov on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
---
+++ Tasker/Views/AppView.swift
@@ -0,0 +1,24 @@
+//
+//  AppView.swift
+//  VOIX
+//
+//  Created by Rodion Akhmedov on 4/10/25.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AppView()
+}
